a05dcb31db92f3d961dd00d8571f470baf1c0e78
Babkin Maksim <mak.babkin.2025@mail.ru>
2026-02-01 15:14:58 +0300

    ADD UserRepository

--- src/main/java/com/javarush/babkin/repository/UserRepository.java
+++ src/main/java/com/javarush/babkin/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package com.javarush.babkin.repository;
 
+
+import com.javarush.babkin.QuestProgress;
 import com.javarush.babkin.entity.Role;
 import com.javarush.babkin.entity.User;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class UserRepository implements Repository<User> {
@@ -15,11 +14,34 @@ public class UserRepository implements Repository<User> {
 
     public static final AtomicLong id = new AtomicLong(System.currentTimeMillis());
 
+
     public UserRepository() {
-        map.put(1L, new User(1L, "ZZTop", "12345", Role.ADMIN));
-        map.put(2L, new User(2L, "Aerosmith", "qwert", Role.USER));
-        map.put(3L, new User(3L, "Ramones", "asdf", Role.GUEST));
+        map.put(1L, User.builder()
+                .id(1L)
+                .login("ZZTop")
+                .password("12345")
+                .role(Role.ADMIN)
+                .questProgress(new QuestProgress())
+                .build());
+
+        map.put(2L, User.builder()
+                .id(2L)
+                .login("Aerosmith")
+                .password("qwert")
+                .role(Role.USER)
+                .questProgress(new QuestProgress())
+                .build());
+
+        map.put(3L, User.builder()
+                .id(3L)
+                .login("Ramones")
+                .password("asdf")
+                .role(Role.GUEST)
+                .questProgress(new QuestProgress())
+                .build());
     }
+
+
 
     @Override
     public Collection<User> getAll() {
