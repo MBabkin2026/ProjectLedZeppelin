77028da637c70bd0abc7f4129d33058ba521e18c
Babkin Maksim <mak.babkin.2025@mail.ru>
2026-02-01 15:14:06 +0300

    MOD User

--- src/main/java/com/javarush/babkin/entity/User.java
+++ src/main/java/com/javarush/babkin/entity/User.java
@@ -1,9 +1,14 @@
 package com.javarush.babkin.entity;
 
+import com.javarush.GameRecord;
+import com.javarush.babkin.PlayerStats;
+import com.javarush.babkin.QuestProgress;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -12,13 +17,31 @@ import lombok.NoArgsConstructor;
 public class User {
 
     private Long id;
-
     private String login;
-
     private String password;
-
     private Role role;
+    private QuestProgress questProgress;
+
+    private int wins;
+    private int losses;
+
+    private PlayerStats stats;
+    private Integer totalGames;
+    private List<GameRecord> gameHistory;
+
+    public int getWins() {
+        return (int) gameHistory.stream()
+                .filter(GameRecord::isWin) // предполагаем поле isWin в GameRecord
+                .count();
+    }
+
+    public int getLosses() {
+        return (int) gameHistory.stream()
+                .filter(game -> !game.isWin()) // не победа = поражение
+                .count();
+    }
 
     public String getImage() {return "image-" + id; }
+
 
 }
